8910d254133a45e363294e9bf443d5e27c0f8438
Glen <glen.goodwin1992@gmail.com>
2023-04-09 14:38:36 -0400

    update alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,14 +3,11 @@ if status is-interactive
 end
 set fish_function_path $fish_function_path ~/plugin-foreign-env/functions
 
-if test -f "/proc/self/cgroup" && grep -s "^0::/user.slice" /proc/self/cgroup > /dev/null
-  # Source Nix setup script
-  fenv source ~/.nix-profile/etc/profile.d/nix.sh
-end
-
+# Source alias and environment Variables
 source ~/.config/fish/aliases
 source ~/.config/fish/environment
 
 starship init fish | source
+zoxide init fish | source
 
 neofetch

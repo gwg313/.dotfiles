db7ccb76ca7a0ef478a133288446011538dd8565
Glen <glen.goodwin1992@gmail.com>
2023-04-09 14:09:23 -0400

    only source nix on host

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,8 +3,10 @@ if status is-interactive
 end
 set fish_function_path $fish_function_path ~/plugin-foreign-env/functions
 
-# Source Nix setup script
-fenv source ~/.nix-profile/etc/profile.d/nix.sh
+if test -f "/proc/self/cgroup" && grep -s "^0::/user.slice" /proc/self/cgroup > /dev/null
+  # Source Nix setup script
+  fenv source ~/.nix-profile/etc/profile.d/nix.sh
+end
 
 source ~/.config/fish/aliases
 source ~/.config/fish/environment

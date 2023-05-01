4607cfffe123478977472cd38d98da55d85c27bd
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-05-01 00:51:43 -0400

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,11 +3,19 @@ if status is-interactive
 end
 set fish_function_path $fish_function_path ~/plugin-foreign-env/functions
 
+set fish_greeting
+
 # Source alias and environment Variables
-source ~/.config/fish/aliases
 source ~/.config/fish/environment
+source ~/.config/fish/aliases
 
 starship init fish | source
 zoxide init fish | source
 
 neofetch
+# if type -q keychain
+#   /usr/bin/keychain $HOME/.ssh/id_ed25519
+#   source $HOME/.keychain/$HOST-sh
+#   eval (keychain --eval --agents ssh -Q --quiet id_rsa --nogui)
+# end
+thefuck --alias | source
